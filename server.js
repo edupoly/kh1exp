afa602bcee3d9c7c3029bb707cc731f6b846abb7
edupoly <info.edupoly@gmail.com>
2022-08-09 13:30:15 +0530

    expressapplication

--- server.js
+++ server.js
@@ -1,1 +1,35 @@
-console.log("hello world")
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser')
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
+
+var students = [];
+
+app.get("/",function(req,res){
+    res.sendFile(__dirname+"/mypage.html")
+})
+
+app.get("/students",function(req,res){
+    console.log(students);
+    res.send(JSON.stringify(students))
+})
+
+app.get("/getRegStuPage",function(req,res){
+    res.sendFile(__dirname+"/addStudent.html")
+})
+
+app.get("/registerStudent",function(req,res){
+    //console.log(req.query);
+    res.send("please wait");
+})
+
+app.post("/registerStudent",function(req,res){
+    students.push(req.body)
+    res.send("thank you for registering with us....")
+})
+
+app.listen(4500)
