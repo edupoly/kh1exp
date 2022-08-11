2853c879fbf2742879defcde6a07bb77b47bcff7
edupoly <info.edupoly@gmail.com>
2022-08-11 19:02:23 +0530

    uploading with storage

--- server.js
+++ server.js
@@ -1,8 +1,24 @@
 var express = require('express')
-var multer = require('multer');
-
-var up = multer({dest:"uploads/"})
+var path = require('path');
 var app = express()
+var bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+var multer = require('multer');
+var storage = multer.diskStorage({
+    destination:function(req,file,cb){
+        console.log(req.body)
+        cb(null,'uploads/'+req.body.firstname+"/")
+    },
+    filename:function(req,file,cb){
+        var x = path.parse(file.originalname)
+        var filename = x.name+Date.now()+x.ext;
+        cb(null,filename)
+    }
+})
+var up = multer({storage:storage})
 app.get("/",(req,res)=>{
     res.sendFile(__dirname+"/mypage.html")
 })
