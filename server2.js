d45a7e5b5b9d43dce2cd10d5de45875eb428c358
edupoly <info.edupoly@gmail.com>
2022-08-12 13:31:33 +0530

    foodImages

--- server2.js
+++ server2.js
@@ -1,9 +1,25 @@
 var fs = require('fs');
+var path = require('path');
 var express = require('express');
 var app = express();
+
 var bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+
+var multer = require('multer');
+var storage = multer.diskStorage({
+    destination:function(req,file,cb){
+        console.log("multer",req.body)
+        cb(null,'public/foodImages/')
+    },
+    filename:function(req,file,cb){
+        var x = path.parse(file.originalname)
+        var filename = x.name+Date.now()+x.ext;
+        cb(null,filename)
+    }
+})
+var up = multer({storage:storage})
 app.use(express.static(__dirname+"/public"))
 const pug = require('pug');
 app.set('view engine', 'pug')
@@ -21,12 +37,14 @@ app.get("/addFoodItem",function(req,res){
     res.sendFile(__dirname+"/public/foodForm.html")
 })
 
-app.post("/addFoodItem",function(req,res){
+app.post("/addFoodItem",up.single('img'),function(req,res){
+    //console.log(req)
+    console.log(req.file)
     console.log(req.body)
     var food = fs.readFileSync('food.txt');
     var foodArray = JSON.parse(food.toString())
-    console.log(foodArray)
-    var newFood = req.body;
+    // console.log(foodArray)
+    var newFood = {...req.body,img:req.file.path};
     foodArray.push(newFood);
     console.log(foodArray)
     fs.writeFileSync('food.txt',JSON.stringify(foodArray))
